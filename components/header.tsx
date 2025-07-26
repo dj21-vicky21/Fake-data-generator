b385f57a69e7b20457c12eee4eada543df5e3a84
dj21-vicky21 <djvicky141@gmail.com>
2025-07-26 16:58:45 +0530

    Update favicon and enhance header component with icon for improved branding

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,13 @@
+import { Croissant, Package2, Squirrel } from 'lucide-react';
 import { ModeToggle } from './mode-toggle';
+
 
 export function Header() {
   return (
     <div className="w-full border-b">
       <div className="max-w-[1400px] p-2 mx-auto flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <h3 className="font-semibold text-sm mt-0.5">Genfaker</h3>
+          <h3 className="font-semibold text-sm mt-0.5 flex gap-2 items-center"><Croissant size={18} />Genfaker</h3>
         </div>
         <div>
           <ModeToggle />
