7f2864925394ea73950a9f559eb5c37dcd8e3ef7
dj21-vicky21 <djvicky141@gmail.com>
2025-07-26 16:40:10 +0530

    Update project name from "fake-data-generator" to "genfaker" in package.json and package-lock.json; modify header component to reflect new name.

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <div className="w-full border-b">
       <div className="max-w-[1400px] p-2 mx-auto flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <h3 className="font-semibold text-sm mt-0.5">FakeData</h3>
+          <h3 className="font-semibold text-sm mt-0.5">Genfaker</h3>
         </div>
         <div>
           <ModeToggle />
